0845ca12052dd9e706fa2eef04abcb88fcfcb24a
Mooteus <moteus10@gmail.com>
2022-04-07 19:12:36 -0300

    fix: title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './style.css';
 
 class Title extends React.Component {
   render() {
     const { headline } = this.props;
+
     return <h2>{ headline }</h2>;
   }
 }
 
-Title.PropTypes = {
+Title.propTypes = {
   headline: PropTypes.string.isRequired,
 };
 
