c6db1a1dd9ff58054d204b5447db0d76135b0da5
Mooteus <moteus10@gmail.com>
2022-04-07 21:01:40 -0300

    feat: add title in solarSystem

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Title from './Title';
 import './style.css';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system" />
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
     );
   }
 }
