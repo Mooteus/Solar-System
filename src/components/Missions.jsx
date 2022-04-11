56202efee1d8ee4569695ee609a75e73729a3ac8
Mooteus <moteus10@gmail.com>
2022-04-11 16:39:01 -0300

    feat: render missions list in missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import MissionCard from './MissionCard';
 import Title from './Title';
+import Data from '../data/missions';
 
 class Missions extends Component {
   render() {
+    const missionData = Data.map((mission) => {
+      const { name, year, country, destination } = mission;
+      return (
+        <MissionCard
+          key={ name }
+          name={ name }
+          year={ year }
+          country={ country }
+          destination={ destination }
+        />
+      );
+    });
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missionData}
       </div>
     );
   }
