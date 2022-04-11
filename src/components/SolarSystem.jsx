b71d675b562fd7095360e183dd085bb86e4a098e
Mooteus <moteus10@gmail.com>
2022-04-11 15:45:49 -0300

    feat: render planets in solarSystem

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Data from '../data/planets';
 import './style.css';
 
 class SolarSystem extends Component {
   render() {
+    const planets = Data.map((planet) => {
+      const { name, image } = planet;
+      return <PlanetCard key={ name } planetName={ name } planetImage={ image } />;
+    });
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <div>
+          { planets }
+        </div>
       </div>
     );
   }
