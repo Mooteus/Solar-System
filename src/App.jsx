3b7f60a26530df17b3e27dd2a33d992060bf191f
Mateus Marçal <moteus10@gmail.com>
2022-09-09 12:29:12 -0300

    Feat: Create Page Home

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import {
   Route,
 } from 'react-router-dom';
 
-import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Header />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
