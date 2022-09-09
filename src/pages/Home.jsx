4693e36471b2c61bb9ab5981230d3034433726fa
Mateus Marçal <moteus10@gmail.com>
2022-09-09 12:46:33 -0300

    Feat: Add stars video to background of Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
+import video from '../assets/stars.mp4';
 
 function Home() {
-  <Header />;
+  return (
+    <>
+      <video autoPlay muted loop id="stars">
+        <source src={video} type="video/mp4" />
+      </video>
+      <Header />
+    </>
+  );
 }
 
 export default Home;
