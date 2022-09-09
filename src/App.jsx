0d0fc90dbf2beae4ae912317b429d370e11d628c
Mateus Marçal <moteus10@gmail.com>
2022-09-09 12:26:44 -0300

    Build: Install Sass and create style files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import './styles/main.scss';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
