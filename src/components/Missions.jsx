e116be3c39b6433e45e7e568c2979c2a5587eca3
Mateus Marçal <moteus10@gmail.com>
2022-09-13 16:46:10 -0300

    Feat: Render Missions Cards in Missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Missions from '../data/missions';
 
 import Title from './Title';
+import MissionCard from './MissionCard';
 
 function Mission() {
   return (
     <div className="missions-container">
       <Title headline="Missões" />
+      {Missions.map(({
+        name, year, country, destination,
+      }) => (
+        <MissionCard name={name} year={year} country={country} destination={destination} />
+      ))}
     </div>
   );
 }
