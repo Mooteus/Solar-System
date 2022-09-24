7bfb88d19bc9b03a967aa5c5dd370fbdf05f5bf5
Mateus Marçal <moteus10@gmail.com>
2022-09-24 03:34:52 -0300

    Test: Verify title in missions component

--- src/tests/Title.test.jsx
+++ src/tests/Title.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import Title from '../components/Title';
 import SolarSystem from '../components/SolarSystem';
+import Missions from '../components/SolarSystem';
 
 describe('Test Title', () => {
   test('01- Verify if props are working', () => {
@@ -13,6 +14,12 @@ describe('Test Title', () => {
   test('02- Verify if title is rendered in the solar system', () => {
     render(<SolarSystem />);
     const title = screen.getByText('Planetas');
+    expect(title).toBeInTheDocument();
+  });
+
+  test('03- Verify if title is rendered in Missions component', () => {
+    render(<Missions />);
+    const title = screen.getByText('Missões');
     expect(title).toBeInTheDocument();
   });
 });
