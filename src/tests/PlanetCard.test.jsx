40d66f6d23f5101d3fbba71e37a767e4938331df
Mateus Marçal <moteus10@gmail.com>
2022-09-24 03:44:24 -0300

    Test: props of planetCard component
    Check planet name and planet image in the Card

--- src/tests/PlanetCard.test.jsx
+++ src/tests/PlanetCard.test.jsx
@@ -8,4 +8,14 @@ describe('Test PlanetCard', () => {
     const planetCard = screen.getByTestId('planet-card');
     expect(planetCard).toBeInTheDocument();
   });
+
+  test('02- Check planet name and planet image in the Card', () => {
+    render(<PlanetCard planetName='Marte' planetImage='imagem'/>);
+
+    const planetName = screen.getByText('Marte');
+    expect(planetName).toBeInTheDocument();
+
+    const planetImage = screen.getByRole('img');
+    expect(planetImage).toHaveAttribute('src', 'imagem');
+  });
 });
